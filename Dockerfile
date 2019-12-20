346a18948a34312e42040bc0ba1760ad13d3743d
Antoine Grondin <antoinegrondin@gmail.com>
2019-12-20 15:32:24 +0900

    build specific versions and tag them

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM golang:1.13
-
+ARG SQLBOILER_VERSION
 ENV GO111MODULE=on
-RUN go get github.com/volatiletech/sqlboiler@v3.5.0
+RUN go get github.com/volatiletech/sqlboiler@v$SQLBOILER_VERSION
 RUN go get github.com/glerchundi/sqlboiler-crdb
--- build.sh
+++ build.sh
@@ -2,15 +2,20 @@
 
 set -eu
 
+sqlboiler_version=$(tr < sqlboiler_version -d '[:space:]')
+
 HUB_USER=$(op get item Docker | jq -r '.details.fields[] | select(.designation=="username").value')
 HUB_PWD=$(op get item Docker | jq -r '.details.fields[] | select(.designation=="password").value')
 
 echo "Logging into Docker Hub"
 echo $HUB_PWD | docker login -u $HUB_USER --password-stdin
 
-echo "Building latest image"
-docker build -t aybabtme/sqlboiler:latest .
+echo "Building $sqlboiler_version image"
+docker build --build-arg SQLBOILER_VERSION=$sqlboiler_version -t aybabtme/sqlboiler:$sqlboiler_version .
+docker tag aybabtme/sqlboiler:$sqlboiler_version aybabtme/sqlboiler:latest
 
 echo "Pushing image to Docker Hub"
+docker push aybabtme/sqlboiler:$sqlboiler_version
 docker push aybabtme/sqlboiler:latest
+docker rmi aybabtme/sqlboiler:$sqlboiler_version
 docker rmi aybabtme/sqlboiler:latest
